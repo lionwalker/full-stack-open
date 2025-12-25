551d736d50529ecba1897d371c2d55583d0879f0
Lahiru Madhusanka <hhlahirumadusanka@gmail.com>
2025-12-25 18:25:25 +0200

    exercise 2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,4 +1,4 @@
-import Part from "./Part";
+import Part from "./Part"
 
 const Content = ({ parts }) => (
     <div>
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,15 @@
-import Header from "./Header";
-import Content from "./Content";
+import Header from "./Header"
+import Content from "./Content"
+import Total from "./Total"
 
 const Course = ({ course }) => {
     return (
         <div>
             <Header course={course.name} />
             <Content parts={course.parts} />
+            <Total total={
+                course.parts.reduce((sum, part) => sum + part.exercises, 0)
+            } />
         </div>
     )
 }
